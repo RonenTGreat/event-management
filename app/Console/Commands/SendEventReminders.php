8a12e691db26a99ecaa5ed09c1d30d5dfbc936af
RonenTGreat <ronenh53@gmail.com>
2025-12-20 13:43:37 +0000

    added reminder notification and job queue

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -30,13 +31,19 @@ class SendEventReminders extends Command
         $events = Event::with('attendees.user')
             ->whereBetween('start_time', [now(), now()->addDay()])
             ->get();
-        
+
         $eventCount = $events->count();
         $eventLabel = Str::plural('event', $eventCount);
 
         $this->info("Found {$eventCount} {$eventLabel}");
 
-        $events->each(fn ($event) => $event->attendees->each(fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")));
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
+            )
+        );
 
 
         $this->info('Reminder notifications sent successfully!');
